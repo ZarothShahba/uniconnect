b079df7f99db4e64a074563ad23e14b24c2f409b
Zaroth Shahba <zarothshahba@gmail.com>
2024-01-26 16:31:35 +0530

    Fix group post to return only group posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,7 +8,6 @@ import mongoose from "mongoose";
 export const createPost = async (req, res) => {
   try {
     const { userId, description, picturePath, videoPath, groupId } = req.body;
-    console.log("🚀 ~ createPost ~ groupId:", groupId);
     const user = await User.findById(userId);
     if (!user) {
       // Handle the case when the user is not found
@@ -29,7 +28,7 @@ export const createPost = async (req, res) => {
     });
     await newPost.save();
 
-    const post = await Post.find();
+    const post = await Post.find({ groupId });
     res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
