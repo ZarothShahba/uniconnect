90217e33750b3893772780224d93e7a66db0a8a9
Zaroth Shahba <zarothshahba@gmail.com>
2024-01-26 21:23:27 +0530

    Fix group list component to hide groups that the user is not a member of

--- client/src/scenes/widgets/GroupList.jsx
+++ client/src/scenes/widgets/GroupList.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemAvatar,
-  Avatar,
-  Typography,
-} from "@mui/material";
+import { Box, List, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import GroupSingle from "components/GroupSingle";
 
 const GroupList = () => {
+  const userId = useSelector((state) => state.user._id);
   const allGroups = useSelector((state) => state.groups);
+
+  // Filter out groups where the current user is not a member
+  const userGroups = allGroups.filter((group) =>
+    group.groupMembers.includes(userId)
+  );
 
   return (
     <Box>
       <Typography variant="h4" mb={2}>
         Groups
       </Typography>
-      <List>
-        {allGroups.map((group) => (
-          <GroupSingle key={group._id} groupId={group._id} />
-        ))}
-      </List>
+      {userGroups.length ? (
+        <List>
+          {userGroups.map((group) => (
+            <GroupSingle key={group._id} groupId={group._id} />
+          ))}
+        </List>
+      ) : (
+        <Typography>You are not a member of any groups.</Typography>
+      )}
     </Box>
   );
 };
